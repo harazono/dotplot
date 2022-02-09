871dd78b2e133d5d697a1ea80b44200132f10b90
harazono <6312081@ed.tus.ac.jp>
2022-02-09 19:29:54 +0900

    discard pio.kaleido.scope

--- dotplot.py
+++ dotplot.py
@@ -457,8 +457,8 @@ def main():
 
 	#fig = draw_dotplot(paf_instance_array, chrm, const, query_annotation = query_gene, ref_annotation = None)
 	fig = draw_dotplot(paf_instance_array, chrm, const, reference_centromere_breakpoint = None, query_annotation = query_gene, reference_annotation = ref_gene)
-	pio.kaleido.scope.default_width = 2400
-	pio.kaleido.scope.default_height = 2400
+	#pio.kaleido.scope.default_width = 2400
+	#pio.kaleido.scope.default_height = 2400
 	if out_file_name is not None:
 		fig.write_image(out_file_name)
 	else:
